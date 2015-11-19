2368393293ada3552dd8089961dfbd8808d30633
Wal33D <aquataze@yahoo.com>
2015-11-19 04:26:37 -0500

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,12 +15,19 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.smtp_settings = {
-    :address => "localhost",
-    :port => 25,
-    :domain => "waleed.com",
-  }
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+:address              => "smtp.gmail.com",
+:port                 => 587,
+:user_name            => 'aquataze91@gmail.com',
+:password             => 'vtcfuumyokxhdhzu',
+:authentication       => 'plain',
+:enable_starttls_auto => true
+}
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
