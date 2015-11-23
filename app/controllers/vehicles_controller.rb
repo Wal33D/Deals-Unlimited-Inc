18e7834b256b826c87c2d02719ac94067f084231
Wal33D <aquataze@yahoo.com>
2015-11-23 01:39:01 -0500

    .

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -79,7 +79,7 @@ class VehiclesController < ApplicationController
        :other_options, :image_1, :image_2, :image_3, :image_4, :image_5, :image_6,
        :image_6, :image_7, :image_8, :image_9, :image_10, :image_11, :image_12,
        :image_13, :image_14, :image_15, :image_16, :image_17, :image_18, :image_19,
-       :image_20, :image_21, :image_22, :image_23, :image_24, {vehicle_images: []}, :vehicle_images, :vehicle_images_cache
+       :image_20, :image_21, :image_22, :image_23, :image_24, {vehicle_images: []}, :vehicle_images, :vehicle_images_cache, :import
 )
     end
 end
