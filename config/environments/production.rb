190a7f9d4888d8cb297c2139963b5f2ed92722d1
Wal33D <aquataze@yahoo.com>
2015-11-26 03:17:21 -0500

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,11 +29,22 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+:address              => "smtp.gmail.com",
+:port                 => 587,
+:user_name            => 'aquataze91@gmail.com',
+:password             => 'vtcfuumyokxhdhzu',
+:authentication       => 'plain',
+:enable_starttls_auto => true
+}
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
