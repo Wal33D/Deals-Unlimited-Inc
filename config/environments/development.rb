202529d21e920052872e03df26d7f69525ff8c50
Wal33D <aquataze@yahoo.com>
2015-12-07 03:58:21 -0500

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,12 +1,12 @@
 
 Rails.application.configure do
 
-  config.cache_classes = true
+  config.cache_classes = false
   config.action_controller.action_on_unpermitted_parameters =:log
-  config.eager_load = true
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-  config.action_mailer.raise_delivery_errors = false
+  config.eager_load = false
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -17,9 +17,5 @@ Rails.application.configure do
     :authentication       => 'plain',
     :enable_starttls_auto => true
 }
-  config.active_support.deprecation = :log
-  config.active_record.migration_error = :page_load
-  config.assets.debug = true
-  config.assets.digest = true
-  config.assets.raise_runtime_errors = true
+
 end
