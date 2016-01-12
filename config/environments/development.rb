8c2dd924d4516b50fc783dc42889e3acc82fde5d
root <root@s97-74-232-132.secureserver.net>
2016-01-12 01:06:39 -0700

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,10 @@
 Rails.application.configure do
-  config.cache_classes = false
+  config.cache_classes = true
   config.action_controller.action_on_unpermitted_parameters =:log
-  config.eager_load = false
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-  config.action_mailer.raise_delivery_errors = true
+  config.eager_load = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -15,5 +15,9 @@ Rails.application.configure do
     :authentication       => 'plain',
     :enable_starttls_auto => true
 }
-  config.assets.raise_runtime_errors = true
+  config.active_support.deprecation = :log
+  config.active_record.migration_error = :page_load
+  config.assets.debug = true
+  config.assets.digest = true
+  config.assets.raise_runtime_errors = false
 end
