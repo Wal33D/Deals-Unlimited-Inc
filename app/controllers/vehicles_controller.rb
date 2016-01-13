760c5b21d8b4d7f10c6fb322579c55df4620f3a1
Waleed H Gudah <Wal33D@Waleeds-MacBook-Air.local>
2016-01-12 23:58:39 -0500

    .

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -34,10 +34,31 @@ end
   def create
     @vehicle = Vehicle.new(vehicle_params)
 
+
     respond_to do |format|
       if @vehicle.save
         format.html { redirect_to @vehicle, notice: 'Vehicle was successfully created.' }
         format.json { render :show, status: :created, location: @vehicle }
+
+        image = MiniMagick::Image.open("public/"+@vehicle.vehicle_images[0].url)
+
+        image.shave '10x55' 
+        image.resize "118x66"
+
+        image.format "png"
+        image.write("public/images/thumbs/admin_thumbs/#{@vehicle.id}.png")  
+        image.write("app/assets/images/thumbs/admin_thumbs/#{@vehicle.id}.png")  
+        @vehicle.update_attributes(:admin_thumb => "thumbs/admin_thumbs/#{@vehicle.id}.png" )
+       
+        image2 = MiniMagick::Image.open("public/"+@vehicle.vehicle_images[0].url)
+
+        image2.shave '0x60' # Removes 46px from top and bottom edges
+        image2.resize "392x293"
+        image2.format "png"
+        image2.write("public/images/thumbs/#{@vehicle.id}.png")  
+        image2.write("app/assets/images/thumbs/#{@vehicle.id}.png")  
+        @vehicle.update_attributes(:thumb => "thumbs/#{@vehicle.id}.png")  
+
       else
         format.html { render :new }
         format.json { render json: @vehicle.errors, status: :unprocessable_entity }
@@ -76,7 +97,7 @@ end
       params.require(:vehicle).permit(:kind, :status, :model, :title, :price,
        :make, :year, :body_style, :mileage, :exterior_color, :interior_color,
        :engine, :transmission, :drive_type, :vehicle_class, :mpg, :trim,
-       :fuel_type, :stereo, :doors, :stock_number, :vin_num, :thumb, :description,
+       :fuel_type, :stereo, :doors, :stock_number, :vin_num, :thumb,:admin_thumb, :description,
        :other_options, :image_1, :image_2, :image_3, :image_4, :image_5, :image_6,
        :image_6, :image_7, :image_8, :image_9, :image_10, :image_11, :image_12,
        :image_13, :image_14, :image_15, :image_16, :image_17, :image_18, :image_19,
