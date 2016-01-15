95672759a9c36d65a8ee55c28b432a2f7975dd46
Waleed H Gudah <Wal33D@Waleeds-MacBook-Air.local>
2016-01-15 05:31:01 -0500

    .

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,6 +6,65 @@ def dashboard
 	#user_thumb_gen
 end
 
+def most_viewed
+	objArray = Array.new
+	@popular = Vehicle.where(:status => "Sale").order(:countclicks => "DESC")
+	
+	@popular.each do |vehicle| 
+		if vehicle.countclicks.is_a? Integer
+			objArray.push vehicle
+		end
+	end
+	
+	@popular = objArray;
+	@popular_first = objArray.first(10).each_slice(5).to_a.first
+ 	@popular_last=  objArray.first(10).each_slice(5).to_a.last
+ 	
+ 	if(!@popular_first)
+		@popular_first = Vehicle.all.order(:countclicks => "DESC").where(:status => "Sale").first(5)
+		@popular_last = Vehicle.all.order(:countclicks => "DESC").where(:status => "Sale").last(5)
+	end
+end
+
+def sale
+	@status_heading = '<div class="panel-heading" style="background-color: #4CAF50 !important;"><h5 id="sold_veh_dash_title">For Sale</h5><div class="panel-title white-text"><h5>Vehicles</h5></div> </div>'
+	@modal_id = 'sale_modal'
+	@vehicle_status = Vehicle.where(:status => "Sale")
+end
+
+def set_sale
+	@vehicle = Vehicle.find(params[:vehicle])
+    @vehicle.update_attributes(:status => "Sale")  
+end
+
+def sold
+	@status_heading = '<div class="panel-heading red"><h5 id="sold_veh_dash_title">Sold</h5><div class="panel-title white-text"><h5>Vehicles</h5></div></div>'
+	@modal_id = 'sold_modal'
+	@vehicle_status = Vehicle.where(:status => "Sold")
+end
+
+def set_sold
+	@vehicle = Vehicle.find(params[:vehicle])
+    @vehicle.update_attributes(:status => "Sold")  
+end
+
+def pending
+	@status_heading = '<div class="panel-heading" style="background-color:#428bca;"><h5 style="float:right; color:white;">Pending</h5><div class="panel-title" style="color:white;"><h5>Vehicles</h5></div> </div>'	
+	@modal_id = 'pending_modal'
+	@vehicle_status = Vehicle.where(:status => "Pending")
+end
+
+def set_pending
+	@vehicle = Vehicle.find(params[:vehicle])
+    @vehicle.update_attributes(:status => "Pending")  
+end
+
+def set_price
+end
+
+def edit_form
+		@vehicle = Vehicle.find(params[:vehicle])
+end
 def admin_thumb_gen
 	Vehicle.all.each do |vehicle| 
 		if vehicle.import == 'import'
@@ -45,59 +104,6 @@ if request.original_url.include? "dash"
 	else
 		flash[:notice]="You are not in the Dashboard"
 	end
-end
-
-def sale
-	
-	@status_heading = '<div class="panel-heading" style="background-color: #4CAF50 !important;"><h5 id="sold_veh_dash_title">For Sale</h5><div class="panel-title white-text"><h5>Vehicles</h5></div> </div>'
-	@modal_id = 'sale_modal'
-	@vehicle_status = Vehicle.where(:status => "Sale")
-end
-
-def set_sale
-	@vehicle = Vehicle.find(params[:vehicle])
-    @vehicle.update_attributes(:status => "Sale")  
-end
-
-def sold
-	@status_heading = '<div class="panel-heading red"><h5 id="sold_veh_dash_title">Sold</h5><div class="panel-title white-text"><h5>Vehicles</h5></div></div>'
-	@modal_id = 'sold_modal'
-	@vehicle_status = Vehicle.where(:status => "Sold")
-end
-
-def set_sold
-	@vehicle = Vehicle.find(params[:vehicle])
-    @vehicle.update_attributes(:status => "Sold")  
-end
-
-def pending
-	@status_heading = '<div class="panel-heading" style="background-color:#428bca;"><h5 style="float:right; color:white;">Pending</h5><div class="panel-title" style="color:white;"><h5>Vehicles</h5></div> </div>'	
-	@modal_id = 'pending_modal'
-	@vehicle_status = Vehicle.where(:status => "Pending")
-end
-
-def set_pending
-	@vehicle = Vehicle.find(params[:vehicle])
-    @vehicle.update_attributes(:status => "Pending")  
-end
-def set_price
-	
-end
-def most_viewed
-	objArray = Array.new
-	@popular = Vehicle.where(:status => "Sale").order(:countclicks => "DESC")
-	@popular.each do |vehicle| 
-		if vehicle.countclicks.is_a? Integer
-			objArray.push vehicle
-		end
-	end
-	@popular = objArray;
-	@popular_first = objArray.first(10).each_slice(5).to_a.first
- 	@popular_last=  objArray.first(10).each_slice(5).to_a.last
- 	if(!@popular_first)
-		@popular_first = Vehicle.all.order(:countclicks => "DESC").where(:status => "Sale").first(5)
-		@popular_last = Vehicle.all.order(:countclicks => "DESC").where(:status => "Sale").last(5)
-end
 end
 
 end
